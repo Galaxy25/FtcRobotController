4e3a88755020569e79c88862f003b6855b2044a4
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-09-14 18:28:44 -0700

    Motor Movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -1,17 +1,30 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "NateTest")
 public class main extends LinearOpMode
 {
+    double up;
     DcMotor motor;
     @Override
     public void runOpMode()
     {
-        telemetry.addData("Initilized", "Successful");
+        motor = hardwareMap.get(DcMotor.class, "motor1");
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        telemetry.addData("Initialized", "Successful");
         telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            up = -this.gamepad1.left_stick_y;
+            motor.setPower(up);
+            telemetry.addData("Joystick", String.valueOf(up));
+            telemetry.update();
+
+        }
     }
 }
