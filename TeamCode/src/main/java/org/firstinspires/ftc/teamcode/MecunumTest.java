b7eb36b9be25d9890b0a768ef65b6269df94d5b8
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-10-26 18:15:00 -0700

    Somewhat Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecunumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecunumTest.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "MecanumTest")
 public class MecunumTest extends LinearOpMode
@@ -15,25 +15,71 @@ public class MecunumTest extends LinearOpMode
         DcMotorSimple leftBack = hardwareMap.get(DcMotorSimple.class, "leftBack");
         DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
         DcMotorSimple rightBack = hardwareMap.get(DcMotorSimple.class, "rightBack");
+        DcMotorSimple arm = hardwareMap.get(DcMotorSimple.class, "arm");
+        CRServo claw = hardwareMap.get(CRServo.class, "claw");
+        CRServo servoPivot = hardwareMap.get(CRServo.class, "servoPivot");
 
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while (opModeIsActive())
         {
-            double leftX = gamepad1.left_stick_x;
             double leftY = gamepad1.left_stick_y;
+            double leftX = gamepad1.left_stick_x;
             double rightX = gamepad1.right_stick_x;
+
+            double drive = leftY;
+            double strafe = leftX;
+            double twist = rightX;
 
             double max = Math.max(Math.abs(leftX) + Math.abs(leftY) + Math.abs(rightX), 1);
 
-            leftFront.setPower((leftY + leftX + rightX) / max);
-            leftBack.setPower((leftY - leftX + rightX) / max);
-            rightFront.setPower((leftY - leftX - rightX) / max);
-            rightBack.setPower((leftY + leftX - rightX) / max);
+            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
+            leftFront.setPower((drive + strafe + twist) / max);
+            rightFront.setPower((drive - strafe - twist) / max);
+            leftBack.setPower((drive - strafe + twist) / max);
+            rightBack.setPower((drive + strafe - twist) / max);
+            telemetry.addData("Left Stick Y: ", gamepad1.left_stick_y);
+            telemetry.addData("Left Stick X: ", gamepad1.left_stick_x);
+            telemetry.addData("Right Stick X: ", gamepad1.right_stick_x);
+            telemetry.addData("Right Stick X: ", gamepad1.right_stick_y);
+            telemetry.update();
+
+             // Claw opening and rotation
+
+            if (gamepad2.left_bumper)
+            {
+                servoPivot.setPower(1 / 2);
+            }
+            else if (gamepad2.left_trigger > 0.8)
+            {
+                servoPivot.setPower(-1 / 2);
+            }
+            else
+            {
+                servoPivot.setPower(0);
+            }
+
+            if (gamepad2.right_bumper)
+            {
+                claw.setPower(1);
+            }
+            else if (gamepad2.right_trigger > 0.8)
+            {
+                claw.setPower(-1 / 2);
+            }
+            else
+            {
+                claw.setPower(0);
+            }
+
+            // Control arm speed
+            if (gamepad2.left_stick_y != 0.0)
+            {
+                arm.setPower(gamepad2.left_stick_y / 2);
+            }
         }
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelDirectionTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "DirectionTest")
+public class WheelDirectionTest extends LinearOpMode {
+    public void runOpMode() {
+        DcMotorSimple leftFront = hardwareMap.get(DcMotorSimple.class, "leftFront");
+        DcMotorSimple leftBack = hardwareMap.get(DcMotorSimple.class, "leftBack");
+        DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
+        DcMotorSimple rightBack = hardwareMap.get(DcMotorSimple.class, "rightBack");
+
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            leftFront.setPower(0.5);
+            leftBack.setPower(0.5);
+            rightFront.setPower(0.5);
+            rightBack.setPower(0.5);
+        }
+    }
+}
