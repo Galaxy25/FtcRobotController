30d242a8c7433b3144d8491f80a65595ea5a326f
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2024-01-25 19:09:22 -0800

    Update MecunumTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecunumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecunumTest.java
@@ -15,9 +15,6 @@ public class MecunumTest extends LinearOpMode
         DcMotorSimple leftBack = hardwareMap.get(DcMotorSimple.class, "leftBack");
         DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
         DcMotorSimple rightBack = hardwareMap.get(DcMotorSimple.class, "rightBack");
-        DcMotorSimple arm = hardwareMap.get(DcMotorSimple.class, "arm");
-        CRServo claw = hardwareMap.get(CRServo.class, "claw");
-        CRServo servoPivot = hardwareMap.get(CRServo.class, "servoPivot");
 
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -34,8 +31,7 @@ public class MecunumTest extends LinearOpMode
             double twist = rightX;
 
             double max = Math.max(Math.abs(leftX) + Math.abs(leftY) + Math.abs(rightX), 1);
-
-            rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            
             rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
             leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -43,45 +39,9 @@ public class MecunumTest extends LinearOpMode
             rightFront.setPower((drive - strafe - twist) / max);
             leftBack.setPower((drive - strafe + twist) / max);
             rightBack.setPower((drive + strafe - twist) / max);
-            telemetry.addData("Left Stick Y: ", gamepad1.left_stick_y);
-            telemetry.addData("Left Stick X: ", gamepad1.left_stick_x);
-            telemetry.addData("Right Stick X: ", gamepad1.right_stick_x);
-            telemetry.addData("Right Stick X: ", gamepad1.right_stick_y);
+            telemetry.addData("Left Trigger: ", gamepad2.left_trigger);
+            telemetry.addData("Right Trigger: ", gamepad2.right_trigger);
             telemetry.update();
-
-             // Claw opening and rotation
-
-            if (gamepad2.left_bumper)
-            {
-                servoPivot.setPower(1 / 2);
-            }
-            else if (gamepad2.left_trigger > 0.8)
-            {
-                servoPivot.setPower(-1 / 2);
-            }
-            else
-            {
-                servoPivot.setPower(0);
-            }
-
-            if (gamepad2.right_bumper)
-            {
-                claw.setPower(1 / 2);
-            }
-            else if (gamepad2.right_trigger > 0.8)
-            {
-                claw.setPower(-1 / 2);
-            }
-            else
-            {
-                claw.setPower(0);
-            }
-
-            // Control arm speed
-            if (gamepad2.left_stick_y != 0.0)
-            {
-                arm.setPower(gamepad2.left_stick_y / 3);
-            }
         }
     }
 
